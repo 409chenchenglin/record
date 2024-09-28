0b1f9a038d6085b5687c65add16068c856145f62
withoutinterrupted <1700090067@qq.com>
2024-09-28 10:00:43 +0800

    Add files via upload

---
+++ 正序分解整数.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	
+	int n,x,a,t;
+	n=1;
+	printf("输入一个正整数");
+	scanf("%d",&x);
+	 a=x;
+	 if(x==0){
+	printf("%d",x);
+	}else{
+ 	while(a>9){
+		a=a/10;
+		n++;
+	}
+}
+	
+	while(x>0){
+		t=pow(10,n-1);
+		a=x/t;
+		x=x%t;
+	n=n-1;
+		printf("%d ",a);
+	}
+	while(n>0){
+		printf("0 ");
+		n--;
+	}
+	printf("\n");
+	return 0;
+
+}
