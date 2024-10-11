eee41c7c17c5d923a820891b3382c313784b8aa2
409chenchenglin <1700090067@qq.com>
2024-10-11 20:42:01 +0800

    Update 自助查询价格.c

--- 409 习题/自助查询价格.c
+++ 409 习题/自助查询价格.c
@@ -3,17 +3,22 @@ int main()
 {
 	int i,n=5;
 	int t;
-	for(i=1;i<=n;i++){
+	int x;
+	scanf("%d",&x);
+	if(x>n){
+		x=n;
+	} 
+	for(i=1;i<=x;i++){
 		scanf("%d",&t);
 		switch(t){
 			case 1:printf("3.0\n");break;
 			case 2:printf("2.5\n");break;
 			case 3:printf("4.0\n");break;
 			case 4:printf("3.5\n");break;
-			case 0:	printf("Thanks"); i=6;break;
+			case 0:i=6;break;
 			default:printf("0.0");break;
 		}
-		printf("Thanks\n");
 	}
+	printf("Thanks\n");
 	return 0;
 }
