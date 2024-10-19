b988040c3a77c26c66b7b3974499b346f086b721
409chenchenglin <1700090067@qq.com>
2024-10-19 17:53:18 +0800

    Update 统计“单词”的个数.c

--- 统计“单词”的个数.c
+++ 统计“单词”的个数.c
@@ -1,20 +1,17 @@
 #include<stdio.h>
 int main()
 {
-	int c;
 	char ch;
-	c=0;
-	ch=getchar();
-	while(ch!='\n'){
-		if(ch!=' '){
-			ch=getchar();
-		}else{
-		ch=getchar();
-		if(ch!=' '){
-			c++;
-		} 
+	int word,cnt;
+	word=cnt=0;
+	while((ch=getchar())!='\n'){
+		if(ch==' '){
+		word=0;
+		}else if(word==0){
+		word=1;
+		cnt++;
+		}
 	}
-}
-	printf("%d",c+1);
+	printf("%d\n",cnt);
 	return 0;
 }
