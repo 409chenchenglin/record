59297bb520f1a5f8c05093b9c7259b2598ec1eb2
409chenchenglin <1700090067@qq.com>
2024-10-27 22:26:57 +0800

    Add files via upload

---
+++ 409 习题/章节10 分治法求解金块问题.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+int max(int a[],int i,int n);
+int min(int a[],int i,int n);
+int main()
+{
+	int i,n;
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("max = %d\n",max(a,0,n-1));
+	printf("min = %d",min(a,0,n-1));
+	return 0;
+}
+
+int max(int a[],int i,int n)
+{
+	int k,result;
+	int r1,r2;
+	if(i==n){
+		result=a[i];
+//		return i;
+	}else{
+		k=(i+n)/2;
+		r1=max(a,i,k);
+		r2=max(a,k+1,n);
+		if(r1>r2){
+			result=r1;
+		}else{
+			result=r2;
+		}
+	}
+	return result;
+}
+
+int min(int a[],int i,int n)
+{
+	int k,result;
+	int r1,r2;
+	if(i==n){
+		result=a[i];
+//		return i;
+	}else{
+		k=(i+n)/2;
+		r1=min(a,i,k);
+		r2=min (a,k+1,n);
+		if(r1>r2){
+			result=r2;
+		}else{
+			result=r1;
+		}
+	}
+	return result;
+}
