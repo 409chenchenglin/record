32de70c3f4aa7a4c338f31e939e92258df810a7f
409chenchenglin <1700090067@qq.com>
2024-11-10 23:03:17 +0800

    Delete python/菜鸟教程100道例题 directory

--- python/菜鸟教程100道例题/15.py
+++
@@ -1,8 +0,0 @@
-#题目：利用条件运算符的嵌套来完成此题：学习成绩>=90分的同学用A表示，60-89分之间的用B表示，60分以下的用C表示
-score=int(input())
-if score>=90:
-    print("A")
-elif 60<=score<=89:
-    print("B")
-else:
-    print("C")
--- python/菜鸟教程100道例题/34.py
+++
@@ -1,8 +0,0 @@
-# 题目：练习函数调用。
-# 程序分析：使用函数，输出三次 RUNOOB 字符串。
-def shucuh():
-    print("RUNOOB")
-
-for i in range(1,3+1):
-    shucuh()
-
