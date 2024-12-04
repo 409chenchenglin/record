776b0df24ea2c194acf5d43b586fb35935dfb556
409chenchenglin <1700090067@qq.com>
2024-12-04 20:16:31 +0800

    Add files via upload

--- 学校作业/结构体 查找最贵和最便宜的书籍.c
+++ 学校作业/结构体 查找最贵和最便宜的书籍.c
@@ -6,7 +6,7 @@ struct book{
 	char title[5];
 	double price;
 	};
-void shuzufuzhi(struct book*yuan,struct book*beifuzhi,int n);
+//void shuzufuzhi(struct book*yuan,struct book*beifuzhi,int n);
 int main()
 {
 	struct book books[10];
@@ -15,29 +15,33 @@ int main()
 		scanf("%s %lf",books[i].title,&books[i].price);
 	}
 	struct book max,min;
-	shuzufuzhi(&(books[0]),&max,strlen(books[0].title));
-	max.price=books[0].price;
-	shuzufuzhi(&(books[0]),&min,strlen(books[0].title));
-	min.price=books[0].price;
+	max=books[0];
+	min=books[0];
+//	shuzufuzhi(&(books[0]),&max,strlen(books[0].title));
+//	max.price=books[0].price;
+//	shuzufuzhi(&(books[0]),&min,strlen(books[0].title));
+//	min.price=books[0].price;
 	for(i=1;i<10;i++){
 		if(max.price<books[i].price){
-			shuzufuzhi(&(books[i]),&max,strlen(books[i].title));
-			max.price=books[i].price;
+			max=books[i];
+//			shuzufuzhi(&(books[i]),&max,strlen(books[i].title));
+//			max.price=books[i].price;
 		}
 		if(books[i].price<min.price){
-			shuzufuzhi(&(books[i]),&min,strlen(books[i].title));
-			min.price=books[i].price;
+			min=books[i];
+//			shuzufuzhi(&(books[i]),&min,strlen(books[i].title));
+//			min.price=books[i].price;
 		}
 	}
-	printf("max:title:%s	price:%.2lf\n",max.title,max.price);
-	printf("min:title:%s	price:%.2lf",min.title,min.price);
+	printf("max:title:%s\tprice:%.2lf\n",max.title,max.price);
+	printf("min:title:%s\tprice:%.2lf",min.title,min.price);
 	return 0;
 }
-void shuzufuzhi(struct book*yuan,struct book*beifuzhi,int n){
-	int i;
-	for (i=0;i<n;i++){
-    	beifuzhi->title[i]=yuan->title[i];
-    }
-    beifuzhi->title[i]='\0';
-	
-}
+//void shuzufuzhi(struct book*yuan,struct book*beifuzhi,int n){
+//	int i;
+//	for (i=0;i<n;i++){
+//    	beifuzhi->title[i]=yuan->title[i];
+//    }
+//    beifuzhi->title[i]='\0';
+//	
+//}
